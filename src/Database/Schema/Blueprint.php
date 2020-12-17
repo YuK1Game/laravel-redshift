c8770f729555752497df812e78119ad25a71ede1
YuK1Game <yuk1.wcat@gmail.com>
2020-12-17 18:37:30 +0900

    feat: support sortkey and distkey

--- src/Database/Schema/Blueprint.php
+++ src/Database/Schema/Blueprint.php
@@ -4,18 +4,56 @@ namespace YuK1\LaravelRedshift\Database\Schema;
 use Illuminate\Database\Schema\Blueprint as IlluminateBlueprint;
 use YuK1\LaravelRedshift\LaravelRedshiftException;
 
+use YuK1\LaravelRedshift\Database\RedshiftConnection as Connection;
+use YuK1\LaravelRedshift\Database\Schema\Grammars\RedshiftGrammar as Grammar;
+
 class Blueprint extends IlluminateBlueprint
 {
+    protected $distStyleType = null;
+
+    protected $distStyleKey = null;
+
+
     public function index($columns, $name = null, $algorithm = null)
     {
         throw new LaravelRedshiftException('Redshift does not support index. Sort key and Dist key are supported.');
     }
 
-    public function sortKey($columns) {
-        // TODO: Add a sort key
+    public function distStyle($style) {
+        return $this->addDistStyle($style);
     }
 
-    public function distKey($columns) {
-        // TODO: Add a dist key
+    public function distKey($key) {
+        return $this->addDistStyle('key', $key);
     }
+
+    public function addDistStyle($type, $key = null) {
+        if ($this->distStyleType) {
+            throw new LaravelRedshiftException('Dist key is already set.');
+        }
+        $this->distStyleType = $type;
+        $this->distStyleKey = $key;
+        return $this;
+    }
+
+    public function hasSortKeyColumn() {
+        return ! is_null(collect($this->getAddedColumns())->first(function ($column) {
+            return $column->sortKey !== null;
+        }));
+    }
+
+    public function sortKeyValues() {
+        if ( ! $this->hasSortKeyColumn()) {
+            return [];
+        }
+
+        return collect($this->getAddedColumns())->mapWithKeys(function ($column) {
+            return [ $column->name => $column->sortKey ];
+        })->filter()->all();
+    }
+
+    public function distKeyValue() {
+        return $this->distStyleType ? [ $this->distStyleType, $this->distStyleKey ] : [];
+    }
+
 }
--- src/Database/Schema/Grammars/RedshiftGrammar.php
+++ src/Database/Schema/Grammars/RedshiftGrammar.php
@@ -4,6 +4,7 @@ namespace YuK1\LaravelRedshift\Database\Schema\Grammars;
 use Illuminate\Database\Schema\Grammars\PostgresGrammar;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Fluent;
+use YuK1\LaravelRedshift\LaravelRedshiftException;
 
 class RedshiftGrammar extends PostgresGrammar
 {
@@ -21,18 +22,7 @@ class RedshiftGrammar extends PostgresGrammar
             ])[$type];
         }
 
-        $options = '';
-
-        if (! is_bool($column->generatedAs) && ! empty($column->generatedAs)) {
-            $options = sprintf(' (%s)', $column->generatedAs);
-        }
-
-        return sprintf(
-            '%s generated %s as identity%s',
-            $type,
-            $column->always ? 'always' : 'by default',
-            $options
-        );
+        return parent::generatableColumn($type, $column);
     }
 
     protected function typeTimestamp(Fluent $column)
@@ -48,4 +38,60 @@ class RedshiftGrammar extends PostgresGrammar
             return ' identity(1,1)';
         }
     }
+
+    public function compileCreate(Blueprint $blueprint, Fluent $command)
+    {
+        return array_values(array_filter(array_merge([sprintf('%s table %s (%s)',
+                $blueprint->temporary ? 'create temporary' : 'create',
+                $this->wrapTable($blueprint),
+                implode(', ', $this->getColumns($blueprint)),
+            )],
+            $this->compileAlter($blueprint)
+        )));
+    }
+
+    protected function compileAlter(Blueprint $blueprint)
+    {
+        $distKey = $this->compileDistKey($blueprint);
+        $sortKey = $this->compileSortKey($blueprint);
+        $keys = collect([ $distKey, $sortKey ])->flatten()->filter()->values();
+        
+        return $keys->map(function($key) use($blueprint) {
+            return sprintf('alter table %s %s', $blueprint->getTable(), $key);
+        })
+        ->values()->all();
+    }
+
+    protected function compileSortKey(Blueprint $blueprint)
+    {
+        if ($values = $blueprint->sortKeyValues()) {
+            return sprintf('alter compound sortkey (%s)',
+                    collect($values)->sort()->keys()->map(function($value) {
+                        return '"' . $value . '"';
+                    })
+                    ->join(', '));
+        }
+        return null;
+    }
+
+    protected function compileDistKey(Blueprint $blueprint)
+    {
+        if ($distKeyColumn = $blueprint->distKeyValue()) {
+            $distKeyColumn = collect($distKeyColumn);
+            
+            switch ($distKeyColumn[0]) {
+                case 'key':
+                    return sprintf('alter diststyle key distkey %s', $distKeyColumn[1]);
+                case 'all':
+                case 'auto':
+                case 'even':
+                    return sprintf('alter diststyle %s', $distKeyColumn[0]);
+                default:
+                    throw new LaravelRedshiftException(sprintf('Dist key name "%s" is not supported.', $distKeyColumn[0]));
+                    
+            }
+        }
+        return null;
+    }
+
 }
