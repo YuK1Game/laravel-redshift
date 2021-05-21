cff24d613201af5a539456345cdb3c08f2f7ba74
Merlijn Buit <merlijn.buit@infotopics.nl>
2021-05-21 17:23:43 +0200

    2 bugfixes

--- src/Database/RedshiftConnection.php
+++ src/Database/RedshiftConnection.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace YuK1\LaravelRedshift\Database;
 
 use Illuminate\Database\PostgresConnection;
@@ -8,37 +9,36 @@ use YuK1\LaravelRedshift\Database\Query\Processors\RedshiftProcessor;
 use YuK1\LaravelRedshift\Database\Schema\Grammars\RedshiftGrammar as SchemaGrammar;
 use YuK1\LaravelRedshift\Database\Schema\RedshiftBuilder;
 use YuK1\LaravelRedshift\Database\Schema\RedshiftSchemaState;
-use Illuminate\Database\Filesystem;
+use Illuminate\Filesystem\Filesystem;
 
 class RedshiftConnection extends PostgresConnection
 {
-    protected function getDefaultQueryGrammar()
-    {
-        return $this->withTablePrefix(new QueryGrammar);
+  protected function getDefaultQueryGrammar()
+  {
+    return $this->withTablePrefix(new QueryGrammar);
+  }
+
+  public function getSchemaBuilder()
+  {
+    if (is_null($this->schemaGrammar)) {
+      $this->useDefaultSchemaGrammar();
     }
 
-    public function getSchemaBuilder()
-    {
-        if (is_null($this->schemaGrammar)) {
-            $this->useDefaultSchemaGrammar();
-        }
+    return new RedshiftBuilder($this);
+  }
 
-        return new RedshiftBuilder($this);
-    }
+  protected function getDefaultSchemaGrammar()
+  {
+    return $this->withTablePrefix(new SchemaGrammar);
+  }
 
-    protected function getDefaultSchemaGrammar()
-    {
-        return $this->withTablePrefix(new SchemaGrammar);
-    }
+  public function getSchemaState(Filesystem $files = null, callable $processFactory = null)
+  {
+    return new RedshiftSchemaState($this, $files, $processFactory);
+  }
 
-    public function getSchemaState(Filesystem $files = null, callable $processFactory = null)
-    {
-        return new RedshiftSchemaState($this, $files, $processFactory);
-    }
-
-    protected function getDefaultPostProcessor()
-    {
-        return new RedshiftProcessor;
-    }
-
+  protected function getDefaultPostProcessor()
+  {
+    return new RedshiftProcessor;
+  }
 }
