be8bba811c50f8af75ff38b2c404adb74de34ee0
Nico van Zijl <nvz.solutions@gmail.com>
2023-07-13 23:13:22 +0200

    Fixed InsertGetId for redshift.

--- src/Database/Query/Grammars/RedshiftGrammar.php
+++ src/Database/Query/Grammars/RedshiftGrammar.php
@@ -2,8 +2,21 @@
 namespace YuK1\LaravelRedshift\Database\Query\Grammars;
 
 use Illuminate\Database\Query\Grammars\PostgresGrammar;
+use Illuminate\Database\Query\Builder;
 
 class RedshiftGrammar extends PostgresGrammar
 {
-
+    /**
+     * Compile insert statement.
+     * Unlike PostgreSQL, in Redshift it is not possible to return the id at the same time
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $values
+     * @param  string  $sequence
+     * @return string
+     */
+    public function compileInsertGetId(Builder $query, $values, $sequence)
+    {
+        return $this->compileInsert($query, $values);
+    }
 }
--- src/Database/Query/Processors/RedshiftProcessor.php
+++ src/Database/Query/Processors/RedshiftProcessor.php
@@ -2,8 +2,30 @@
 namespace YuK1\LaravelRedshift\Database\Query\Processors;
 
 use Illuminate\Database\Query\Processors\PostgresProcessor;
+use Illuminate\Database\Query\Builder;
 
 class RedshiftProcessor extends PostgresProcessor
 {
+    /**
+     * Process an "insert get ID" query.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  string|null  $sequence
+     * @return int
+     */
+    public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+    {
+        $connection = $query->getConnection();
 
+        $connection->insert($sql, $values);
+
+        $idColumn = $sequence ?: 'id';
+        $wrappedTable = $query->getGrammar()->wrapTable($query->from);
+        $result = $connection->selectOne('select max('. $idColumn .') from '.$wrappedTable);
+        $id = array_values((array) $result)[0];
+
+        return is_numeric($id) ? (int) $id : $id;
+    }
 }
